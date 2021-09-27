2dd435819aedfad6c3e92b9e1fe1aea19a69b1d2
Tuấn Nam <tuannam2505@gmail.com>
2021-09-27 21:33:42 +0700

    Update 2

---
+++ assets/js/notify/index.js
@@ -0,0 +1,12 @@
+﻿'use strict';
+var notify = $.notify('<i class="fa fa-bell-o"></i><strong>Đang tải</strong> không đóng trang này...', {
+    type: 'theme',
+    allow_dismiss: true,
+    delay: 2000,
+    showProgressbar: true,
+    timer: 300
+});
+
+setTimeout(function() {
+    notify.update('message', '<i class="fa fa-bell-o"></i><strong>Đang tải</strong> dữ liệu bên trong.');
+}, 1000);
