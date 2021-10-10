24b2b91f988f940f6a1ad49470622ac9628f8f41
Tuấn Nam <tuannam2505@gmail.com>
2021-10-10 16:19:10 +0700

    nam lam them moi danh muc

--- Default.aspx.cs
+++ Default.aspx.cs
@@ -7,8 +7,26 @@ using System.Web.UI.WebControls;
 
 public partial class _Default : Page
 {
+    public class Student
+    {
+       public string FirstName { get; set; }
+       public string LastName { get; set; }
+       public int Age { get; set; }
+    }
+    public List<Student> listsv = new List<Student>();
+
+   
     protected void Page_Load(object sender, EventArgs e)
     {
-
+        Student[] student = new Student[10];
+        for (int i = 0; i < 10; i++)
+        {
+            student[i] = new Student();
+            student[i].FirstName = "Pham "+i;
+            student[i].LastName = "Hoang "+i;
+            student[i].Age = 1+i;
+            listsv.Add(student[i]);
+        }
+       
     }
 }
